fdb15f206b9f8f25469a8743fb6c58d5f683e371
TariqKichawele <tariqkichawele66@gmail.com>
2024-08-04 13:44:55 -0400

    project completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRoutes from './routes/product.route.js';
 import { connectDB } from './config/db.js';
 
@@ -8,8 +9,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "fronted", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
